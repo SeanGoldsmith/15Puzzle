aef887398fe7e6cefee2f50321e9a229e92586fa
Sean Goldsmith <goldsmith.sean90@gmail.com>
2021-04-21 14:28:20 -0700

    Added color picker for text and tile color.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+var colorPicker = new iro.ColorPicker('#picker', {width: 100});
+
+
 let moveable = [ [2,5],[1,6,3],[2,7,4],
                   [3,8],[1,6,9],[2,5,7,10],[3,6,8,11],
                   [4,7,12],[5,10,13],[6,9,11,14],[7,10,12,15],
@@ -74,4 +77,39 @@ function scramble() {
     for (i=0;i<40;i++) {
         randomMove();
     }
+}
+
+function photoToggle() {
+    let tiles = document.getElementsByClassName('tile');
+    let tilesArray = Array.from(tiles);
+    if (tiles[0].classList.contains('photo-back')) {
+        tilesArray.forEach(element => {
+            element.classList.remove('photo-back')
+        });
+    }
+    else {
+        tilesArray.forEach(element => {
+            element.classList.add('photo-back')
+        });
+    }
+}
+
+function selectTextColor() {
+    var hex = colorPicker.color.hexString;
+    let tileArray = Array.from(document.getElementsByClassName('tile'));
+    tileArray.forEach(element => {
+        element.style.color=hex;
+    });   
+}
+
+function selectTileColor() {
+    var hex = colorPicker.color.hexString;
+    let tileArray = Array.from(document.getElementsByClassName('tile'));
+    tileArray.forEach(element => {
+        if(element.id!=="emptyTile"){
+            console.log(element.id)
+            element.style.backgroundColor=hex;
+        }
+        
+    });   
 }
